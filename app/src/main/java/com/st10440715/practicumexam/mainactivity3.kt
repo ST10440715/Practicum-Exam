9ba35b59e54c27dd4f4c93ea9b0e15e63633fbdd
ST10440715 <st10440715@vcconnect.edu.za>
2024-06-10 13:17:53 +0200

    third edit

--- app/src/main/java/com/st10440715/practicumexam/mainactivity3.kt
+++ app/src/main/java/com/st10440715/practicumexam/mainactivity3.kt
@@ -1,18 +1,48 @@
 
 package com.st10440715.practicumexam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class mainactivity3 : AppCompatActivity() {
-    private var etDayOfWeek: EditText? = null
-    private var 
+    private var etDay: EditText? = null
+    private var etMiniTemp: EditText? = null
+    private var etMaxiTemp: EditText? = null
+    private var tvAverage: TextView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainactivity3)
 
 
+        etDay = findViewById(R.id.etDay)
+        etMiniTemp = findViewById(R.id.etMiniTemp)
+        etMaxiTemp = findViewById(R.id.etMaxiTemp)
+        tvAverage = findViewById(R.id.tvAverage)
+
+        val btnAverage = findViewById<Button>(R.id.btnAve)
+        btnAverage.setOnClickListener {
+            calculateTheAverage()
+        }
+
+        val btnExit = findViewById<Button>(R.id.btnExit)
+        btnExit.setOnClickListener {
+            Intent(this, MainActivity::class.java)
+            startActivity(intent)       //back to splash screen
+        }
     }
+
+    private fun calculateTheAverage() {
+        val miniTemp = etMiniTemp?.text.toString().toIntOrNull() ?: 0
+        val maxiTemp = etMaxiTemp?.text.toString().toIntOrNull() ?: 0
+
+        val average = (miniTemp + maxiTemp) / 2
+        tvAverage?.text = "Average Temperature: $average"
+    }
+
 }
